1c452a4a7e406468e14ecc7a6f0c601872b4a106
Hayeong Pyeon <hayeong.pyeon@gmail.com>
2024-03-04 14:05:03 -0800

    add GetAll and Find func + passing tests

--- Tracker.Tests/ModelTests/OrderTests.cs
+++ Tracker.Tests/ModelTests/OrderTests.cs
@@ -71,5 +71,39 @@ namespace Tracker.Tests
 
       Assert.AreEqual(price, result);
     }
+
+    [TestMethod]
+    public void GetAll_ReturnsAllOrderObjects_OrderList()
+    {
+      string order01 = "Pastries";
+      string order02 = "Garlic Bread";
+      string date = "Oct 01, 2023";
+      string price = "70";
+
+      Order newOrder1 = new Order(order01, date, price);
+      Order newOrder2 = new Order(order02, date, price);
+      List<Order> newList = new List<Order> { newOrder1, newOrder2 };
+
+      List<Order> result = Order.GetAll();
+
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void Find_ReturnsCorrectVendor_Vendor()
+    {
+      string order01 = "Pastries";
+      string order02 = "Garlic Bread";
+      string date = "Oct 01, 2023";
+      string price = "70";
+
+      Order newOrder1 = new Order(order01, date, price);
+      Order newOrder2 = new Order(order02, date, price);
+
+      Order result = Order.Find(2);
+
+      Assert.AreEqual(newOrder2, result);
+    }
+
   }
 }
--- Tracker/Models/Order.cs
+++ Tracker/Models/Order.cs
@@ -20,5 +20,15 @@ namespace Tracker.Models
     {
       _instances.Clear();
     }
+
+    public static List<Order> GetAll()
+    {
+      return _instances;
+    }
+
+    public static Order Find(int searchId)
+    {
+      return _instances[searchId - 1];
+    }
   }
 }
