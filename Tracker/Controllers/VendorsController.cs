c9c56c98b042f52b2432c039403080e131c2a05b
Hayeong Pyeon <hayeong.pyeon@gmail.com>
2024-03-04 13:16:53 -0800

    add controller and views for Vendor

--- Tracker/Controllers/VendorsController.cs
+++ Tracker/Controllers/VendorsController.cs
@@ -7,6 +7,35 @@ namespace Tracker.Controllers
 {
   public class VendorsController : Controller
   {
-    
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName, string registerDate, string address)
+    {
+      Vendor newVendor = new Vendor(vendorName, registerDate, address);
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object> {};
+      Vendor selectedVendor = Vendor.Find(id);
+      
+      model.Add("vendor", selectedVendor);
+
+      return View(model);
+    }
   }
 }
